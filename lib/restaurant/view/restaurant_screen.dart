738dc9b87f63448358429bfe2ab2bc3ba8669160
starshb <gksquf7367@naver.com>
2023-08-04 23:26:26 +0900

    Container

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -5,6 +5,10 @@ class RestaurantScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Text('홈'),
+      ),
+    );
   }
 }
